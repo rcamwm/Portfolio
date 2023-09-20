63d3f8854207ce357dac1727f7cd4ef1d10c856b
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2023-09-19 19:28:07 -0700

    New project

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div>
+      <nav className="flex items-center justify-center py-4 font-courier-new">
+        <a href="localhost:3000" class="mx-2">Link 1</a>
+        <a href="localhost:3000" class="mx-2">Link 2</a>
+        <a href="localhost:3000" class="mx-2">Link 3</a>
+      </nav>
+    </div>
+
+  )
+}
+
+export default Header;
