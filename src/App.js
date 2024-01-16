e591624321f9400f2075de2a2d3f640bfa921ec6
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2024-01-15 16:09:09 -0800

    fixed eth redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import { Redirect, Routes, Route, useLocation } from "react-router-dom";
+import { React, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./Pages/Header";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Projects from "./Pages/Projects";
 import Contact from "./Pages/Contact";
 import ANNimator from "./Pages/ANNimator";
+
+const EthRedirectComponent = () => {
+  useEffect(() => {
+    window.location.href = 'https://colab.research.google.com/drive/1ZrAy71aptI1kfU9V41mpVqy2gAOtU-dJ?usp=sharing';
+  }, []);
+  return null;
+};
 
 function App() {
   const currPath = useLocation().pathname;
@@ -37,9 +44,10 @@ function App() {
           </div>
         } />
 
-        <Route path="/ETH_Price_Prediction">
-          <Redirect to="https://colab.research.google.com/drive/1ZrAy71aptI1kfU9V41mpVqy2gAOtU-dJ?usp=sharing"/>
-        </Route>
+        <Route
+          path="/ETH_Price_Prediction"
+          element={<EthRedirectComponent />}
+        />
 
         <Route path="/ANNimator" element={
           <div className="main-bg">
